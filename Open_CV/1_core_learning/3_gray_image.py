0da9049381ecca16c844e09e12aef69099fd65d0
Arshvir <avarshvir@gmail.com>
2024-09-16 14:25:22 +0530

    commit

--- Open_CV/1_core_learning/3_gray_image.py
+++ Open_CV/1_core_learning/3_gray_image.py
@@ -1,7 +1,9 @@
 # color to grey image
 import cv2
 img = cv2.imread("../resources/images/img4.jpg")
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray Image", gray)
+gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#cv2.imshow("Color Image", img)
+cv2.imshow("Gray Image", gray_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
