20bd68a27a9b12143290564c3b714526cf8614de
Arshvir <avarshvir@gmail.com>
2024-09-16 14:07:54 +0530

    commit

--- Open_CV/1_core_learning/2_resing_image.py
+++ Open_CV/1_core_learning/2_resing_image.py
@@ -1,7 +1,9 @@
 # Resizing Image
 import cv2
 img = cv2.imread("../resources/images/img4.jpg")
-img = cv2.resize(img,(1200,680))
-cv2.imshow("Image Resize", img)
+img1 = cv2.resize(img,(1200,680))
+
+#cv2.imshow("Image Original", img)
+cv2.imshow("Image Resize", img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
