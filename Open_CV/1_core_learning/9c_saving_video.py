4dd62aff7081f284f39528845a651a6662e2d99b
Arshvir <avarshvir@gmail.com>
2024-09-22 16:32:06 +0530

    commit

---
+++ Open_CV/1_core_learning/9c_saving_video.py
@@ -0,0 +1,13 @@
+'''
+import cv2
+
+cap = cv2.VideoCapture('../resources/videos/video1.mp4')
+
+if(cap.isOpened() == False):
+    print("Error")
+
+while():
+    isColor = True
+    out.release()
+
+'''
