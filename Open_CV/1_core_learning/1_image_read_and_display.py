f9191d9770fea84363ecdcc836cbebe714ba364b
avarshvir <avarshvir@gmail.com>
2025-04-16 16:11:41 +0530

    computer vision

--- Open_CV/1_core_learning/1_image_read_and_display.py
+++ Open_CV/1_core_learning/1_image_read_and_display.py
@@ -1,6 +1,8 @@
 # Reading an image and displaying it
 import cv2
 img = cv2.imread("../resources/images/img4.jpg")
+print(img.shape)
+print(type(img))
 cv2.imshow("image 1", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
